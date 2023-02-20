8b24e65036719abe3ef427abc82cb35df30e6ee7
Yangcheng He <107044023+hyc1230@users.noreply.github.com>
2023-02-20 23:10:38 +0800

    Update Site

--- alert.js
+++ alert.js
@@ -1,5 +1,5 @@
-var ver = "1.1.0";
-var alert = `热烈庆祝 LGSE 正式版本发布！<br>当前最新版本：${ver}<br>访问量统计：<img src="https://badges.toozhao.com/badges/01GSN1QZ8F7H2RWHEQEZC7YY4F/blue.svg" />`;
+var ver = "1.1.1";
+var alert = `公告：热烈庆祝 LGSE 正式版本发布！<br>当前最新版本：<span style="color: #e67e22">${ver}</span><br>访问量统计：<img src="https://badges.toozhao.com/badges/01GSN1QZ8F7H2RWHEQEZC7YY4F/blue.svg" />`;
 function set() {
-    document.getElementById("alert").innerHTML = `<p>公告：${alert}</p>`;
+    document.getElementById("alert").innerHTML = `<p>${alert}</p>`;
 }
