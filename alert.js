0088082820ad44df6a773a78f675d5b7a189d75f
hycqwq <107044023+hyc1230@users.noreply.github.com>
2023-12-24 14:02:38 +0800

    fix

--- alert.js
+++ alert.js
@@ -1,6 +1,6 @@
-var alert = `<p><i class="fa fa-flag-o"></i> 公告：热烈庆祝 LGSE 正式版本发布！<br>
+var alert = `<p><i class="fa fa-flag"></i> 公告：热烈庆祝 LGSE 正式版本发布！<br>
 <i class="fa fa-code"></i> 当前最新版本：<span style="color: #e67e22" id="version">正在获取中...</span><br>
-<i class="fa fa-envelope-o"></i> 访问量统计：<img src="https://badges.toozhao.com/badges/01GSN1QZ8F7H2RWHEQEZC7YY4F/blue.svg" style="height: 1em;" /></p>`;
+<i class="fa fa-envelope"></i> 访问量统计：<img src="https://badges.toozhao.com/badges/01GSN1QZ8F7H2RWHEQEZC7YY4F/blue.svg" style="height: 1em;" /></p>`;
 var bar = `&nbsp;&nbsp;&nbsp;
 <span class="BarElement"><i class="fa fa-list"></i> 导航</span>
 &nbsp;&nbsp;|&nbsp;&nbsp;
